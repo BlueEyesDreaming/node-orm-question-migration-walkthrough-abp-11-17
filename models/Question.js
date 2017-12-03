df085748562f6a3d8565e157a4a389e4b8b78354
Ashton Reed <dreamergirl411@gmail.com>
2017-12-03 17:57:55 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -1,17 +1,26 @@
 'use strict';
 
-const db = require("./config/db")
+const db = require("../config/db")
 
 class Question{
   static CreateTable() {
-    return new Promise(function(resolve) {
-      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY)`
-      db.run(sql, function(){
-        resolve("questions table created")
-      })
-    }
   }
 }
 
-
 module.exports = Question;
+//
+// const db = require("../config/db")
+//
+// class Question{
+//   static CreateTable() {
+//     return new Promise(function(resolve) {
+//       const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY)`
+//       db.run(sql, function(){
+//         resolve("questions table created")
+//       })
+//     }
+//   }
+// }
+//
+//
+// module.exports = Question;
